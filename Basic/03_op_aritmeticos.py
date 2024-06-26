38769b52670696631c59ed354f1a1c51412c1bab
bmsjale93 <117013312+bmsjale93@users.noreply.github.com>
2024-06-26 17:34:25 +0200

    Operadores Aritméticos

---
+++ Basic/03_op_aritmeticos.py
@@ -0,0 +1,27 @@
+### OPERADORES ARITMÉTICOS ###
+
+# SUMA Y RESTA
+suma = 12 + 5
+resta = 12 - 5
+
+# MULTIPLICACIÓN Y DIVISIÓN
+multiplicacion = 12 * 5
+division = 12 / 5   # Devuelve un float
+
+# EXPONENTES
+exponente = 12 ** 5
+
+# DIVISIÓN ENTERA
+division_entera = 12 // 5 # Devuelve un entero (Redondea hacia abajo)
+
+# MÓDULO (resto de la división)
+modulo = 12 % 5
+
+print(suma) # 17
+print(resta) # 7
+print(multiplicacion) # 60
+print(division) # 2.4
+print(exponente) # 248832
+print(division_entera) # 2
+print(modulo) # 2
+
