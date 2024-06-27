5c258c759dc56d51f919ff407136fb1b3d076ac8
bmsjale93 <117013312+bmsjale93@users.noreply.github.com>
2024-06-27 17:20:43 +0200

    Ajustes

---
+++ Basic/06_op_logicos.py
@@ -0,0 +1,23 @@
+### OPERADORES LÓGICOS ###
+# Los operadores lógicos nos permiten realizar comparaciones entre valores.
+# Los operadores lógicos son:
+# - and: Devuelve True si ambos valores son True
+# - or: Devuelve True si al menos uno de los valores es True
+# - not: Devuelve True si el valor es False y viceversa
+
+# AND
+Resultado = True & True # True
+Resultado = True & False # False
+Resultado = False & True # False
+Resultado = False & False # False
+
+#OR
+Resultado = True | True # True
+Resultado = True | False # True
+Resultado = False | True # True
+Resultado = False | False # False
+
+#NOT
+Resultado = not True # False
+Resultado = not False # True
+
