ca52f5a3c34e59f24358c2f49f1b090a7af4b656
bmsjale93 <117013312+bmsjale93@users.noreply.github.com>
2024-07-09 17:36:22 +0200

    Funciones con Datos Extra

---
+++ Funciones/03_funciones_datos_extra.py
@@ -0,0 +1,35 @@
+### FUNCIONES CON DATOS EXTRA ###
+
+print("-------------------")
+def frase(nombre, apellido, adjetivo):
+    return f"Hola {nombre} {apellido}, eres muy {adjetivo}"
+
+frase_resultado = frase("Alex", "Delgado", "inteligente")
+print(frase_resultado)
+print("-------------------")
+
+# Podemos forzar a que los argumentos sean pasados en el orden correcto
+def frase(nombre, apellido, adjetivo):
+    return f"Hola {nombre} {apellido}, eres muy {adjetivo}"
+
+frase_resultado = frase(adjetivo="inteligente", nombre="Alex", apellido="Delgado")
+print(frase_resultado)
+print("-------------------")
+
+# También podemos forzar los argumentos al pasar los parámetros
+def frase(nombre, apellido, adjetivo = "tonto"):
+    return f"Hola {nombre} {apellido}, eres muy {adjetivo}"
+
+frase_resultado = frase("Alex", "Delgado")
+print(frase_resultado)
+print("-------------------")
+
+# Estos parámetros que pasamos por la función serían los valores por defecto
+# Esto podemos modificarlo en la llamada de la función
+def frase(nombre, apellido, adjetivo="tonto"):
+    return f"Hola {nombre} {apellido}, eres muy {adjetivo}"
+
+
+frase_resultado = frase("Alex", "Delgado", "inteligente")
+print(frase_resultado)
+print("-------------------")
