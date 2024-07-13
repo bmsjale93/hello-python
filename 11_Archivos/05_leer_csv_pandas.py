68c37979d624557dac5f533bb5f41dc52d8234d1
bmsjale93 <117013312+bmsjale93@users.noreply.github.com>
2024-07-13 15:29:17 +0200

    Leer CSV con Pandas + Slicing

--- 11_Archivos/05_leer_csv_pandas.py
+++ 11_Archivos/05_leer_csv_pandas.py
@@ -40,10 +40,64 @@ df_concatenado = pd.concat([df, df2])
 print(df_concatenado)
 print("-------------------")
 
+# Podemos acceder a la primera fila con head() y a la última con tail()
+primera_fila = df.head(1)
+print(primera_fila)
+print("-------------------")
 
+# También podemos acceder a las 3 primeras filas o las que queramos
+primera_fila = df.head(3)
+print(primera_fila)
+print("-------------------")
+
+# Por otro lado, podemos acceder a las 3 últimas filas o las que queramos
+ultimas_filas = df.tail(3)
+print(ultimas_filas)
+print("-------------------")
+
+# También podemos acceder a la cantidad de filas y columnas de un dataframe con la función shape
+filas_y_columnas = df.shape
+print(filas_y_columnas)
+print("-------------------")
+
+# Como devuelve una tupla, podemos desempaquetarla
+filas, columnas = df.shape
+print(f"Las filas totales son: {filas}\nLas columnas totales son: {columnas}")
+print("-------------------")
+
+# De forma más estadística, podemos utilizar la función describe()
+estadisticas = df.describe()
+print(estadisticas)
+print("-------------------")
 
 # SLICING
 # Podemos seleccionar un rango de filas y columnas con slicing
 cadena = "0123456789"
 print(cadena[0:5])  # 01234
+print("-------------------")
 
+# Con el slicing podemos acceder a elementos específicos con loc[]
+elemento_especifico = df.loc[0, 'nombre']
+print(elemento_especifico)
+print("-------------------")
+
+# Otra forma de acceder a elementos específicos es con iloc[]
+elemento_especifico = df.iloc[0, 0]
+print(elemento_especifico)
+print("-------------------")
+
+# Además, podemos acceder a todas las filas de una columna específica
+# De la siguiente forma, le decimos que acceda a todos los elementos de la columna 1
+columna_especifica = df.iloc[:,1]
+print(columna_especifica)
+print("-------------------")
+
+# También funciona al revés y podemos acceder a todas las columnas de una fila específica
+fila_especifica = df.iloc[2,:]
+print(fila_especifica)
+print("-------------------")
+
+# Podemos establecer condiciones, como seleccionar todas las filas donde la edad sea mayor a 25
+condicion = df.loc[df["edad"]>25,:]
+print(condicion)
+print("-------------------")
