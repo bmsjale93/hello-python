5b44f004efa86f5cde02178eb3d8b396a7ebe20b
bmsjale93 <117013312+bmsjale93@users.noreply.github.com>
2024-07-17 18:47:24 +0200

    Trabajando con Clases y Objetos

---
+++ 16_POO/00_Clases_y_Objetos/00_clases_objetos.py
@@ -0,0 +1,65 @@
+### CREANDO CLASES Y OBJETOS ###
+
+# Una clase es un molde para crear objetos. Los objetos son instancias de una clase.
+# Las clases tienen atributos (características) y métodos (acciones).
+# Para definir una clase, usamos la palabra reservada "class".
+# Para crear un objeto de una clase, usamos la palabra reservada "self".
+
+# CREANDO UNA CLASE Y UN OBJETO ESTÁTICOS
+# Crearemos una clase para construir un tipo de teléfono. Este tendrá los datos estáticos.
+class Celular():
+    marca = "Samsung"
+    modelo = "Galaxy A10"
+    camara = "48MP"
+
+# Instanciamos la clase
+celular1 = Celular()
+celular2 = Celular()
+celular3 = Celular()
+celular4 = Celular()
+
+# Accedemos a los atributos de la clase llamando al objeto y el atributo
+print("--------------------")
+print(f"Con datos estáticos:\n{celular1.marca}\n{celular2.marca}\n{celular3.marca}\n{celular4.marca}")
+print("--------------------")
+
+
+# CREANDO CLASES Y OBJETOS ESTÁTICOS DINÁMICOS
+class Celular:
+    def __init__(self, marca, modelo, camara):
+        self.marca = marca
+        self.modelo = modelo
+        self.camara = camara
+        
+celular1 = Celular("Apple", "iPhone 12", "12MP")
+celular2 = Celular("Samsung", "Galaxy A10", "48MP")
+celular3 = Celular("Xiaomi", "Redmi Note 9", "48MP")
+celular4 = Celular("Motorola", "Moto G9", "48MP")
+
+print(f"Con datos dinámicos:\n{celular1.marca}\n{celular2.marca}\n{celular3.marca}\n{celular4.marca}")
+print("--------------------")
+
+
+# LAS CLASES PUEDEN ALBERGAR MÉTODOS
+class Celular:
+    # El método __init__ es un método constructor que se ejecuta al instanciar la clase
+    def __init__(self, marca, modelo, camara):
+        self.marca = marca
+        self.modelo = modelo
+        self.camara = camara
+    
+    # Los métodos llamar( y cortar() son métodos que simulan una acción
+    def llamar(self):
+        return f"Llamando desde un {self.modelo}"
+    
+    def cortar(self):
+        return f"Cortando llamada desde un {self.modelo}"
+
+
+celular1 = Celular("Apple", "iPhone 12", "12MP")
+celular2 = Celular("Samsung", "Galaxy A10", "48MP")
+celular3 = Celular("Xiaomi", "Redmi Note 9", "48MP")
+celular4 = Celular("Motorola", "Moto G9", "48MP")
+
+print(f"Realizando la acción de llamar y colgar:\n{celular1.llamar()}\n{celular1.cortar()}")
+print("--------------------")
